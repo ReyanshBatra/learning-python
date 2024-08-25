a0da076fce65cc5864a17aab1c9910938a7481f0
ReyanshBatra <107682959+ReyanshBatra@users.noreply.github.com>
2024-08-25 16:18:14 +0530

    Add files via upload

---
+++ py5.py
@@ -0,0 +1,3 @@
+print("Hey I am a \"good boy\"\nand")
+print("Hey",6, 7,sep="-",end="009\n")
+print("reyansh")
---
+++ py6.py
@@ -0,0 +1,20 @@
+a = 1
+b = "blah blah"
+c = 1.23456
+d = complex(8 + 2j)
+
+print(a, b, sep="\n")
+print(a + c)
+print("the type of a is", type(a))
+print("the type of b is", type(b))
+print("the type of c is", type(c))
+print("the type of d is", type(d))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Reyansh", "age":20, "canVote":True}
+print(dict1)
